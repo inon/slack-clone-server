a999522c6aa1086ca012df2a872e2d9c03f8f832
Inon Baguio <inon.baguio@yahoo.com>
2018-09-01 21:12:05 +0800

    installing dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress} from 'apollo-server-express';
+
+const PORT = 8080;
+const app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+
+app.listen(PORT);
